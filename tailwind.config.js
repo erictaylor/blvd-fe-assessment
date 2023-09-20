83a6bbebc1e5101bf9cd944a344a9794bdb8b7db
Eric Taylor <eric@daxos.com>
2023-09-20 14:12:41 -0600

    chore: add tailwind forms setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import formsPlugin from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
-	plugins: [],
+	plugins: [formsPlugin],
 };
